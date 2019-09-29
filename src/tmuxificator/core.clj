4e5fbedec68613e5f38c64948a4e656666a355d7
ronen <narkisr@gmail.com>
2019-09-29 19:23:19 +1000

    basic functionality is implemented

--- src/tmuxificator/core.clj
+++ src/tmuxificator/core.clj
@@ -1,25 +1,58 @@
 (ns tmuxificator.core
   (:gen-class)
   (:require
-   [clojure.java.shell :as sh]))
+   [clojure.java.io :as io]
+   [clojure.edn :as edn]
+   [clojure.core.strint :refer  (<<)]
+   [clojure.java.shell :refer (with-sh-dir sh)]))
+
+(defn stderr [e]
+  (binding [*out* *err*]
+    (println e)))
+
+(defn exit [c]
+  (System/exit c))
+
+(defn configuration
+  ([]
+   (let [f (str (System/getProperty "user.home") "/.tmx.edn")]
+     (when-not (.exists (io/file f))
+       (stderr (<< "~{f} configuration file is missing"))
+       (exit 1))
+     (edn/read-string (slurp f))))
+  ([& ks] (get-in (configuration) ks)))
 
 (defn help []
   (println "
 Usage:
-  tmx start  {profile}  - start a tmux profile
+  tmx start {profile}  - start a tmux profile
   tmx version         - print and check latest version.
   tmx help            - print this help message.
 "))
 
+(defn start-tmux [profile root {:keys [dir cmd]}]
+  (sh (configuration :terminal) "-e" (<< "/usr/bin/tmux new-session -s ~{profile} ~{cmd}") :dir (or dir root)))
+
+(defn launch
+  [profile]
+  {:pre [(string? profile)]}
+  (let [{:keys [root windows]} (configuration :profiles (keyword profile))
+        base (first windows)]
+    (start-tmux profile root base)
+    (doseq [{:keys [cmd dir]} (rest windows)]
+      (sh "/usr/bin/tmux" "new-window" "-t" (<< "~{profile}:0") cmd :dir (or dir root)))))
+
 (defn -main [& args]
   (try
     (case (first args)
-      "start" (println "starting")
+      "start" (launch (second args))
       "help" (help)
       nil (help))
     (catch Exception e
-      (println e)
-      (System/exit
+      (stderr e)
+      (exit
+
+
 
        1))))
 
